15ac60e6eb8db419e50fd6501ea697fe2fce1999
Marlene Ganslmeier <ganslmeier.marlene@gmail.com>
2023-01-11 13:33:29 +0100

    "this version works until 93"

--- screening_test_resolve_bioscience.py
+++ screening_test_resolve_bioscience.py
@@ -5,7 +5,7 @@
 import random #technically a library but I feel it's too much to ask to write your own randomness algorithm
 #it also comes with the standard installation so I think it count's as acceptable 
 import csv #same applies
-
+import numpy
 #function to create a random string comprised of the 4 bases 
 def create_seq():
     bases = ["A", "T", "G", "C"]
@@ -23,31 +23,51 @@ def hamming_distance(str1, str2):
 
 def check(list_size):
     words = []
+    tested = []
     count = 0
+    repeats = 0
+
     while len(words) < list_size:
         seq = create_seq()
-        count += 1
-        dist = []
         
-        if len(words) == 0:
-            words.append(seq)
+        
+        if seq in tested:
+            repeats += 1
+        
         else:
-            for word in words:
-                x = hamming_distance(seq, word)
-                dist.append(x)
-
-            match = min(dist)
-            if match >= 3: #add this to other for loop later
+            tested.append(seq)
+            count += 1
+            dist = []
+            
+            if len(words) == 0:
                 words.append(seq)
-    
+            else:
+                for word in words:
+                    x = hamming_distance(seq, word)
+                    print(word, x)
+                    if x <3:
+                        #print("abort loop")
+                        break
+                    dist.append(x)
+                #print(dist)
+
+                if len(dist) == len(words): #add this to other for loop later
+                    words.append(seq)
+        
          
     
-    print(count)
+    # print("tested ", tested)
+    # print("lenghts", len(tested))
+    # print("unique", numpy.unique(tested))
+    # print("unique length", len(numpy.unique(tested)))
+    # print("count ", count)
+    # print("repeats", repeats)
     return words
             
-test = check(10)
+test = check(50)
 test_words = ["".join(x) for x in test]
 print(test_words)
+print("lenghts", len(test_words))
 
 out = csv.writer(open("words.txt","w"), delimiter='\n')
 out.writerow(test_words)
